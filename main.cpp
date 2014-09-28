90cb07ff73929efe13f83e1af32930dd9604eab2
gryllos <gryllosprokopis@gmail.com>
2014-09-28 04:09:19 +0300

    Create main file
    It is still empty though

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* Gryllos Prokopis 2013-2014
+ * Implementation of dynamic incremental algorithm for minimal length paths
+ * [AIMN91] as described in the Journal of Algorithms 12 (1991), pp.615-638.
+ * main file for testing data structure.
+ */
+#include "aimn91.h"
+
+int main(){
+    /* create a sample graph with no edges and test data structure */
+    return 0;
+}
