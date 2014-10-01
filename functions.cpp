e9e78668ae2d8f52d9bc0f623bec61f5482ea432
gryllos <gryllosprokopis@gmail.com>
2014-10-01 05:49:29 +0300

    Remove unnecessary helper function

--- functions.cpp
+++ functions.cpp
@@ -6,42 +6,6 @@
 
 #include "aimn91.h"
 // add edge (i,j) with edge weight w
-void DistanceMap::test(Graph& g, v_desc i,v_desc h){
-   // Tree anc_i,anc_h;
-   // Tree anc_h;
-    std::cout<<boost::any_cast<v_desc>(g[i].ANC[B[std::make_pair(i,i)]].v_g)<<std::endl;
-    std::cout<<boost::any_cast<v_desc>(g[i].ANC[F[std::make_pair(h,h)]].v_g)<<std::endl;
-   // v_desc h;
-//  int count=0;
-//  std::pair<vertex_it, vertex_it> vi4;
-//  count = 0;
-//  for (vi4=boost::vertices(g); vi4.first != vi4.second; ++vi4.first){
-//      std::cout<<anc_h[F[std::make_pair(h,h)]].v_g<<std::endl;
-//      if(count == 6){
-//          //h = *vi4.first;
-//            Tree anc_h = ANC[h];
-//      }
-//      count++;
-//  }
-//
-//  //vi4=boost::vertices(g);
-//std::pair<tree_vertex_it, tree_vertex_it> vi2;
-//std::pair<vertex_it, vertex_it> vi3;
-//for (vi3=boost::vertices(g); vi3.first != vi3.second; ++vi3.first){
-//    Tree test2 = DESC[*vi3.first];
-//    std::cout<<"root "<<test2[F[std::make_pair(*vi3.first,*vi3.first)]].v_g<<std::endl;
-//    for (vi2=boost::vertices(test2); vi2.first != vi2.second; ++vi2.first){
-//        std::cout<<*vi2.first<<" "<<test2[*vi2.first].v_g<<std::endl;
-//    }
-//}
-////std::pair<vertex_it, vertex_it> vi2;
-////for (vi2=boost::vertices(g); vi2.first != vi2.second; ++vi2.first){
-////    Tree test = ANC[*vi2.first];
-////    vertex_tree root = boost::any_cast<vertex_tree>(test[boost::graph_bundle].root);
-////    std::cout<<*vi2.first<<" "<<test[root].v_g<<std::endl;
-////}
-    return;
-}
 void DistanceMap::add(v_desc i, v_desc j, int w, Graph& g){
     vertex_tree x = B[std::make_pair(i,i)];
     vertex_tree root = F[std::make_pair(j,j)];
